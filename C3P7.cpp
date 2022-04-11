2c3c7ee172d112b33a15e1bf25013cf9df411d46
shyjs3 <51873759+shyjs3@users.noreply.github.com>
2022-04-11 13:34:10 +0800

    Add files via upload

---
+++ C3P7.cpp
@@ -0,0 +1,270 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+// ndn-congestion-topo-plugin.cpp
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/ndnSIM-module.h"
+
+namespace ns3 {
+
+/**
+ * This scenario simulates a grid topology (using topology reader module)
+ *
+ *   /------\                                                    /------\
+ *   | Src1 |<--+                                            +-->| Dst1 |
+ *   \------/    \                                          /    \------/
+ *                \                                        /
+ *                 +-->/------\   "bottleneck"  /------\<-+
+ *                     | Rtr1 |<===============>| Rtr2 |
+ *                 +-->\------/                 \------/<-+
+ *                /                                        \
+ *   /------\    /                                          \    /------\
+ *   | Src2 |<--+                                            +-->| Dst2 |
+ *   \------/                                                    \------/
+ *
+ * To run scenario and see what is happening, use the following command:
+ *
+ *     NS_LOG=ndn.Consumer:ndn.Producer ./waf --run=ndn-congestion-topo-plugin
+ */
+
+ //C3P7
+
+int
+main(int argc, char* argv[])
+{
+  CommandLine cmd;
+  cmd.Parse(argc, argv);
+
+  AnnotatedTopologyReader topologyReader("", 25);
+  topologyReader.SetFileName("src/ndnSIM/examples/topologies/C3P9.txt");
+  topologyReader.Read();
+
+  // Install NDN stack on all nodes
+  ndn::StackHelper ndnHelper;
+  // ndnHelper.SetOldContentStore("ns3::ndn::cs::Lru", "MaxSize", "10000");
+  ndnHelper.InstallAll();
+
+
+
+  // Choosing forwarding strategy
+  ndn::StrategyChoiceHelper::InstallAll("/", "/localhost/nfd/strategy/best-route");
+
+  // Installing global routing interface on all nodes
+  ndn::GlobalRoutingHelper ndnGlobalRoutingHelper;
+  ndnGlobalRoutingHelper.InstallAll();
+
+  // Getting containers for the consumer/producer
+  Ptr<Node> consumer1 = Names::Find<Node>("Doc1");
+  Ptr<Node> consumer2 = Names::Find<Node>("Doc2");
+  Ptr<Node> consumer3 = Names::Find<Node>("Doc3");
+
+  Ptr<Node> producer1 = Names::Find<Node>("Dev1Pat1");
+  Ptr<Node> producer2 = Names::Find<Node>("Dev2Pat1");
+  Ptr<Node> producer3 = Names::Find<Node>("Dev3Pat1");
+  Ptr<Node> producer4 = Names::Find<Node>("Dev1Pat2");
+  Ptr<Node> producer5 = Names::Find<Node>("Dev2Pat2");
+  Ptr<Node> producer6 = Names::Find<Node>("Dev3Pat2");
+  Ptr<Node> producer7 = Names::Find<Node>("Dev1Pat3");
+  Ptr<Node> producer8 = Names::Find<Node>("Dev2Pat3");
+  Ptr<Node> producer9 = Names::Find<Node>("Dev3Pat3");
+  Ptr<Node> producer10 = Names::Find<Node>("Dev1Pat4");
+  Ptr<Node> producer11 = Names::Find<Node>("Dev2Pat4");
+  Ptr<Node> producer12 = Names::Find<Node>("Dev3Pat4");
+  Ptr<Node> producer13 = Names::Find<Node>("Dev1Pat5");
+  Ptr<Node> producer14 = Names::Find<Node>("Dev2Pat5");
+  Ptr<Node> producer15 = Names::Find<Node>("Dev3Pat5");
+  Ptr<Node> producer16 = Names::Find<Node>("Dev1Pat6");
+  Ptr<Node> producer17 = Names::Find<Node>("Dev2Pat6");
+  Ptr<Node> producer18 = Names::Find<Node>("Dev3Pat6");
+  Ptr<Node> producer19 = Names::Find<Node>("Dev1Pat7");
+  Ptr<Node> producer20 = Names::Find<Node>("Dev2Pat7");
+  Ptr<Node> producer21 = Names::Find<Node>("Dev3Pat7");
+
+  ndn::AppHelper consumerHelper("ns3::ndn::ConsumerHealth");
+  consumerHelper.SetAttribute("Frequency", StringValue("5")); // 5 interests a second
+  consumerHelper.SetAttribute("Randomize", StringValue("exponential"));
+
+  // on the first consumer node install a Consumer application
+  // that will express interests in /dst1 namespace
+  consumerHelper.SetAttribute("Seed", StringValue("3"));
+  consumerHelper.SetPrefix("/Pat1_2");
+  consumerHelper.Install(consumer1);
+  
+  consumerHelper.SetAttribute("Seed", StringValue("7"));
+  consumerHelper.SetPrefix("/Pat3_4");
+  consumerHelper.Install(consumer2);
+
+
+  consumerHelper.SetAttribute("Seed", StringValue("5"));
+  consumerHelper.SetPrefix("/Pat5_6_7");
+  consumerHelper.Install(consumer3);
+
+
+  ndn::AppHelper producerHelper("ns3::ndn::HealthProducer");
+  producerHelper.SetAttribute("PayloadSize", StringValue("1018"));
+
+  // Register /dst1 prefix with global routing controller and
+  // install producer that will satisfy Interests in /dst1 namespace
+  ndnGlobalRoutingHelper.AddOrigins("/Pat1_2", producer1);
+  producerHelper.SetPrefix("/Pat1_2");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("1"));
+  producerHelper.Install(producer1);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat1_2", producer2);
+  producerHelper.SetPrefix("/Pat1_2");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("3"));
+  producerHelper.Install(producer2);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat1_2", producer3);
+  producerHelper.SetPrefix("/Pat1_2");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("2"));
+  producerHelper.Install(producer3);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat1_2", producer4);
+  producerHelper.SetPrefix("/Pat1_2");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("1"));
+  producerHelper.Install(producer4);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat1_2", producer5);
+  producerHelper.SetPrefix("/Pat1_2");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("5"));
+  producerHelper.Install(producer5);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat1_2", producer6);
+  producerHelper.SetPrefix("/Pat1_2");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("2"));
+  producerHelper.Install(producer6);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat3_4", producer7);
+  producerHelper.SetPrefix("/Pat3_4");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("4"));
+  producerHelper.Install(producer7);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat3_4", producer8);
+  producerHelper.SetPrefix("/Pat3_4");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("5"));
+  producerHelper.Install(producer8);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat3_4", producer9);
+  producerHelper.SetPrefix("/Pat3_4");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("3"));
+  producerHelper.Install(producer9);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat3_4", producer10);
+  producerHelper.SetPrefix("/Pat3_4");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("1"));
+  producerHelper.Install(producer10);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat3_4", producer11);
+  producerHelper.SetPrefix("/Pat3_4");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("3"));
+  producerHelper.Install(producer11);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat3_4", producer12);
+  producerHelper.SetPrefix("/Pat3_4");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("2"));
+  producerHelper.Install(producer12);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat5_6_7", producer13);
+  producerHelper.SetPrefix("/Pat5_6_7");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("1"));
+  producerHelper.Install(producer13);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat5_6_7", producer14);
+  producerHelper.SetPrefix("/Pat5_6_7");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("5"));
+  producerHelper.Install(producer14);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat5_6_7", producer15);
+  producerHelper.SetPrefix("/Pat5_6_7");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("2"));
+  producerHelper.Install(producer15);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat5_6_7", producer16);
+  producerHelper.SetPrefix("/Pat5_6_7");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("4"));
+  producerHelper.Install(producer16);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat5_6_7", producer17);
+  producerHelper.SetPrefix("/Pat5_6_7");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("5"));
+  producerHelper.Install(producer17);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat5_6_7", producer18);
+  producerHelper.SetPrefix("/Pat5_6_7");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("3"));
+  producerHelper.Install(producer18);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat5_6_7", producer19);
+  producerHelper.SetPrefix("/Pat5_6_7");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("3"));
+  producerHelper.Install(producer19);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat5_6_7", producer20);
+  producerHelper.SetPrefix("/Pat5_6_7");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("3"));
+  producerHelper.Install(producer20);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat5_6_7", producer21);
+  producerHelper.SetPrefix("/Pat5_6_7");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("3"));
+  producerHelper.Install(producer21);
+
+  // Calculate and install FIBs
+  ndn::GlobalRoutingHelper::CalculateRoutes();
+
+  Simulator::Stop(Seconds(50.0));
+
+  ndn::AppDelayTracer::InstallAll("app-delays.txt");
+  Simulator::Run();
+  Simulator::Destroy();
+
+  return 0;
+}
+
+} // namespace ns3
+
+int
+main(int argc, char* argv[])
+{
+  return ns3::main(argc, argv);
+}
---
+++ C6P3.cpp
@@ -0,0 +1,203 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2011-2015  Regents of the University of California.
+ *
+ * This file is part of ndnSIM. See AUTHORS for complete list of ndnSIM authors and
+ * contributors.
+ *
+ * ndnSIM is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * ndnSIM is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * ndnSIM, e.g., in COPYING.md file.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+// ndn-congestion-topo-plugin.cpp
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/ndnSIM-module.h"
+
+namespace ns3 {
+
+/**
+ * This scenario simulates a grid topology (using topology reader module)
+ *
+ *   /------\                                                    /------\
+ *   | Src1 |<--+                                            +-->| Dst1 |
+ *   \------/    \                                          /    \------/
+ *                \                                        /
+ *                 +-->/------\   "bottleneck"  /------\<-+
+ *                     | Rtr1 |<===============>| Rtr2 |
+ *                 +-->\------/                 \------/<-+
+ *                /                                        \
+ *   /------\    /                                          \    /------\
+ *   | Src2 |<--+                                            +-->| Dst2 |
+ *   \------/                                                    \------/
+ *
+ * To run scenario and see what is happening, use the following command:
+ *
+ *     NS_LOG=ndn.Consumer:ndn.Producer ./waf --run=ndn-congestion-topo-plugin
+ */
+
+ //C6P3
+
+int
+main(int argc, char* argv[])
+{
+  CommandLine cmd;
+  cmd.Parse(argc, argv);
+
+  AnnotatedTopologyReader topologyReader("", 25);
+  topologyReader.SetFileName("src/ndnSIM/examples/topologies/C3P9.txt");
+  topologyReader.Read();
+
+  // Install NDN stack on all nodes
+  ndn::StackHelper ndnHelper;
+  // ndnHelper.SetOldContentStore("ns3::ndn::cs::Lru", "MaxSize", "10000");
+  ndnHelper.InstallAll();
+
+
+
+  // Choosing forwarding strategy
+  ndn::StrategyChoiceHelper::InstallAll("/", "/localhost/nfd/strategy/best-route");
+
+  // Installing global routing interface on all nodes
+  ndn::GlobalRoutingHelper ndnGlobalRoutingHelper;
+  ndnGlobalRoutingHelper.InstallAll();
+
+  // Getting containers for the consumer/producer
+  Ptr<Node> consumer1 = Names::Find<Node>("Doc1");
+  Ptr<Node> consumer2 = Names::Find<Node>("Doc2");
+  Ptr<Node> consumer3 = Names::Find<Node>("Doc3");
+  Ptr<Node> consumer4 = Names::Find<Node>("Doc4");
+  Ptr<Node> consumer5 = Names::Find<Node>("Doc5");
+  Ptr<Node> consumer6 = Names::Find<Node>("Doc6");
+
+  Ptr<Node> producer1 = Names::Find<Node>("Dev1Pat1");
+  Ptr<Node> producer2 = Names::Find<Node>("Dev2Pat1");
+  Ptr<Node> producer3 = Names::Find<Node>("Dev3Pat1");
+  Ptr<Node> producer4 = Names::Find<Node>("Dev1Pat2");
+  Ptr<Node> producer5 = Names::Find<Node>("Dev2Pat2");
+  Ptr<Node> producer6 = Names::Find<Node>("Dev3Pat2");
+  Ptr<Node> producer7 = Names::Find<Node>("Dev1Pat3");
+  Ptr<Node> producer8 = Names::Find<Node>("Dev2Pat3");
+  Ptr<Node> producer9 = Names::Find<Node>("Dev3Pat3");
+
+
+  ndn::AppHelper consumerHelper("ns3::ndn::ConsumerHealth");
+  consumerHelper.SetAttribute("Frequency", StringValue("5")); // 5 interests a second
+  consumerHelper.SetAttribute("Randomize", StringValue("exponential"));
+
+  // on the first consumer node install a Consumer application
+  // that will express interests in /dst1 namespace
+  consumerHelper.SetAttribute("Seed", StringValue("3"));
+  consumerHelper.SetPrefix("/Pat1");
+  consumerHelper.Install(consumer1);
+  
+  consumerHelper.SetAttribute("Seed", StringValue("7"));
+  consumerHelper.SetPrefix("/Pat2");
+  consumerHelper.Install(consumer2);
+
+
+  consumerHelper.SetAttribute("Seed", StringValue("5"));
+  consumerHelper.SetPrefix("/Pat3");
+  consumerHelper.Install(consumer3);
+
+  consumerHelper.SetAttribute("Seed", StringValue("3"));
+  consumerHelper.SetPrefix("/Pat1");
+  consumerHelper.Install(consumer4);
+  
+  consumerHelper.SetAttribute("Seed", StringValue("7"));
+  consumerHelper.SetPrefix("/Pat2");
+  consumerHelper.Install(consumer5);
+
+
+  consumerHelper.SetAttribute("Seed", StringValue("5"));
+  consumerHelper.SetPrefix("/Pat3");
+  consumerHelper.Install(consumer6);
+
+  ndn::AppHelper producerHelper("ns3::ndn::HealthProducer");
+  producerHelper.SetAttribute("PayloadSize", StringValue("1018"));
+
+  // Register /dst1 prefix with global routing controller and
+  // install producer that will satisfy Interests in /dst1 namespace
+  ndnGlobalRoutingHelper.AddOrigins("/Pat1", producer1);
+  producerHelper.SetPrefix("/Pat1");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("1"));
+  producerHelper.Install(producer1);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat1", producer2);
+  producerHelper.SetPrefix("/Pat1");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("3"));
+  producerHelper.Install(producer2);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat1", producer3);
+  producerHelper.SetPrefix("/Pat1");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("2"));
+  producerHelper.Install(producer3);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat2", producer4);
+  producerHelper.SetPrefix("/Pat2");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("1"));
+  producerHelper.Install(producer4);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat2", producer5);
+  producerHelper.SetPrefix("/Pat2");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("5"));
+  producerHelper.Install(producer5);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat2", producer6);
+  producerHelper.SetPrefix("/Pat2");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("2"));
+  producerHelper.Install(producer6);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat3", producer7);
+  producerHelper.SetPrefix("/Pat3");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("4"));
+  producerHelper.Install(producer7);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat3", producer8);
+  producerHelper.SetPrefix("/Pat3");
+  producerHelper.SetAttribute("DataType", StringValue("2"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("5"));
+  producerHelper.Install(producer8);
+
+  ndnGlobalRoutingHelper.AddOrigins("/Pat3", producer9);
+  producerHelper.SetPrefix("/Pat3");
+  producerHelper.SetAttribute("DataType", StringValue("4"));
+  producerHelper.SetAttribute("DiseaseRank", StringValue("3"));
+  producerHelper.Install(producer9);
+
+
+  // Calculate and install FIBs
+  ndn::GlobalRoutingHelper::CalculateRoutes();
+
+  Simulator::Stop(Seconds(50.0));
+
+  ndn::AppDelayTracer::InstallAll("app-delays.txt");
+  Simulator::Run();
+  Simulator::Destroy();
+
+  return 0;
+}
+
+} // namespace ns3
+
+int
+main(int argc, char* argv[])
+{
+  return ns3::main(argc, argv);
+}
